67f0d88af597a354abf298bc0acc81a924d991e4
an-lime <andrej.gorbunov.2006@mail.ru>
2025-01-18 16:55:17 +0300

    added the ability to send intended letters

--- handlers/game_action.py
+++ handlers/game_action.py
@@ -9,7 +9,7 @@ from databases.models import Words
 from filters.game_filter import check_word
 from keyboards.game_button import create_inline_game_keyboard
 from middlewares.db_data import WordMiddleware
-from services.change_data import get_alphabet
+from services.change_data import get_alphabet, get_word_from_callback, get_letter_from_callback, try_guess_letter
 from lexicon.body import display_hangman
 from states.botStates import BotStates
 
@@ -19,19 +19,25 @@ router_game_action.callback_query.middleware(WordMiddleware())
 
 @router_game_action.callback_query(F.data == 'start_game', StateFilter(default_state))
 async def start_game(callback: CallbackQuery, hidden_word: Words, db: Database, state: FSMContext):
-
     await state.update_data({'hidden_word': hidden_word})
+    await state.update_data({'word_completion': "□" * len(hidden_word.word)})
+    await state.update_data({'try': 6})
+
     del hidden_word
     hidden_word: Words = await state.get_value('hidden_word')
-    keyboard = create_inline_game_keyboard(8, **get_alphabet(word=hidden_word.word))
+    word_completion: str = await state.get_value('word_completion')
+    tries: int = await state.get_value('try')
+
+    alphabet = await get_alphabet(word=hidden_word.word)
+    keyboard = create_inline_game_keyboard(8, **alphabet)
 
     await db.user_in_game.add_game(id_user=str(callback.from_user.id), id_word=hidden_word.id_word, session=db.session)
 
     await callback.message.edit_text(
         text=f'{hidden_word.word} \n'
-             f'{display_hangman(6)} \n'
+             f'{display_hangman(tries)} \n'
              f'{hidden_word.description} \n'
-             f'{"□" * len(hidden_word.word)} ',
+             f'{word_completion} ',
         reply_markup=keyboard
     )
     await state.set_state(BotStates.in_game)
@@ -44,10 +50,34 @@ async def try_start_game(callback: CallbackQuery):
 
 @router_game_action.callback_query(StateFilter(BotStates.in_game))
 async def game_letter(callback: CallbackQuery, state: FSMContext):
-
     hidden_word: Words = await state.get_value('hidden_word')
+    word_completion: str = await state.get_value('word_completion')
+    tries: int = await state.get_value('try')
+
+    if callback.data[-1] == '*':
+        print(123)
+        await callback.answer()
+        return
+
     if check_word(word=hidden_word.word, callback=callback):
-        print(hidden_word.word)
-        await callback.answer(callback.data)
+
+        if get_letter_from_callback(callback=callback).lower() in hidden_word.word.lower():
+            word_completion = try_guess_letter(letter=get_letter_from_callback(callback=callback).lower(),
+                                               word=hidden_word.word, word_completion=word_completion)
+            await state.update_data({'word_completion': word_completion})
+
+            alphabet = await get_alphabet(word=hidden_word.word, del_letter=word_completion)
+            keyboard = create_inline_game_keyboard(8, **alphabet)
+
+            await callback.message.edit_text(
+                text=f'{hidden_word.word} \n'
+                     f'{display_hangman(tries)} \n'
+                     f'{hidden_word.description} \n'
+                     f'{word_completion} ',
+                reply_markup=keyboard
+            )
+        else:
+            await callback.answer('такой буквы нет')
+
     else:
-        await callback.answer('нононо')
+        await callback.answer('Данная игра не актуальна!')
--- services/change_data.py
+++ services/change_data.py
@@ -1,11 +1,41 @@
 from aiogram.types import CallbackQuery
 
 
-def get_alphabet(word: str) -> dict[str, str]:
+async def get_alphabet(word: str, del_letter: str = None) -> dict[str, str]:
     alpha = {f'char_{chr(character)}_{word}': f'{chr(character)}' for character in range(ord('А'), ord('Е') + 1)}
     alpha.update({f'char_Ё_{word}': 'Ё'})
     alpha.update({f'char_{chr(character)}_{word}': f'{chr(character)}' for character in range(ord('Ж'), ord('Я') + 1)})
+    if del_letter is not None:
+        del_letter = del_letter.replace('_', '')
+
+        for key in list(alpha.keys()):
+            if key.split('_')[1].lower() in del_letter:
+                alpha[f'{key}*'] = alpha.pop(key)
+                alpha = dict(sorted(alpha.items(), key=lambda x: x[1]))
+
+                mykeys = list(alpha.keys())
+                myitems = list(alpha.values())
+                mykeys.insert(6, mykeys.pop(0))
+                myitems.insert(6, myitems.pop(0))
+                alpha = dict(zip(mykeys, myitems))
+
+        for key in list(alpha.keys()):
+            if key[-1] == '*':
+                alpha[key] = '*'
+
     return alpha
+
 
 def get_word_from_callback(callback: CallbackQuery):
     return callback.data.split('_')[-1]
+
+
+def get_letter_from_callback(callback: CallbackQuery):
+    return callback.data.split('_')[1]
+
+
+def try_guess_letter(letter: str, word: str, word_completion: str) -> str:
+    for i in range(len(word)):
+        if word[i] == letter:
+            word_completion = word_completion[:i] + letter + word_completion[i + 1:]
+    return word_completion
